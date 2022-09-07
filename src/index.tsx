6507cb9c020ae65190a8bc559069894be0809a18
Chris Bentivenga <chris@chrisbenti.com>
2022-09-06 22:29:05 -0700

    Cleaning Up Template

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./reset.css";
 import "./index.css";
-import App from "./App";
+import { Index } from "./pages/index";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route index element={<Index />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export const Index = () => {
+  return <h1> Welcome </h1>;
+};
