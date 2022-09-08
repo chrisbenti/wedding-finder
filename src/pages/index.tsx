0ce3576e4140b309aabb39c24b962272226e9bb7
Chris Bentivenga <chris@chrisbenti.com>
2022-09-07 23:38:15 -0700

    background not bad

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,9 +97,9 @@ const Content = styled.div`
     left: 0;
     bottom: 0;
     right: 0;
-    height: 100%;
-    width: 100%;
-    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
     z-index: -1;
   }
 `;
