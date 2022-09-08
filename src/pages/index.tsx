1e2540919031875e03c7c7fa39bdb912ff7d3e92
Chris Bentivenga <chris@chrisbenti.com>
2022-09-07 18:23:54 -0700

    pretty linking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,75 @@
-import path from "path";
 import { useState, useCallback, useEffect } from "react";
 import { computePath } from "../model/familyDataGetter";
 import { familyGraph, RelationshipPath } from "../model/familyGraph";
-import { css } from "@emotion/react";
+import { Relation } from "../model/familyGraph";
 
 import styled from "@emotion/styled";
 import { useBackgroundColor, useBodyColor } from "../hooks/useBackgroundColor";
+
 import { Navigation } from "../components/navbar";
 import { NameInput } from "../components/nameInput";
 import { GRAY_RANGE } from "../colors";
 
+const LINE_COLOR = "green";
+const FACE_SIZE_PX = 40;
+const NodeBox = styled.div``;
+
+const NodePerson = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LINE_WIDTH_PX = 4;
+const NodeRelationship = styled.div`
+  text-align: center;
+  border-left: solid ${LINE_WIDTH_PX}px ${LINE_COLOR};
+
+  // This math is to make it so that the vertical line
+  // is centered to the face circle.
+  // FACE_TOTAL_SIZE / 2 + L/2 from the left
+  // where FACE_TOTAL_SIZE = FACE_SIZE + LINE_WIDTH * 2
+  // tl;dr it's centered by the power of geometry
+  margin: 0 ${(FACE_SIZE_PX + LINE_WIDTH_PX) / 2}px;
+
+  padding: 0 2rem 1rem;
+
+  font-size: 1.1rem;
+`;
+
+const Face = styled.div`
+  height: ${FACE_SIZE_PX}px;
+  width: ${FACE_SIZE_PX}px;
+  border: solid ${LINE_WIDTH_PX}px ${LINE_COLOR};
+  border-radius: 100%;
+  margin-right: 0.5rem;
+`;
+const Node = ({ relation: r }: { relation: Relation }) => (
+  <NodeBox>
+    <NodePerson>
+      <Face />
+      <div>{r.sourceName}</div>
+    </NodePerson>
+    <NodeRelationship>
+      is the {r.relationship.replace("-", " ")}
+    </NodeRelationship>
+  </NodeBox>
+);
+
+const TerminalNode = ({ relation: r }: { relation: Relation }) => (
+  <NodeBox>
+    <NodePerson>
+      <Face />
+      <div>{r.targetName}</div>
+    </NodePerson>
+  </NodeBox>
+);
+
 const Results = ({ path }: { path: RelationshipPath }) => (
   <div>
-    Connection
     {path.relations.map((elem) => (
-      <div>
-        {elem.sourceName} {elem.relationship}
-      </div>
+      <Node relation={elem} />
     ))}
-    <div>{path.relations.at(-1)?.targetName}</div>
+    <TerminalNode relation={path.relations.at(-1)!} />
   </div>
 );
 
